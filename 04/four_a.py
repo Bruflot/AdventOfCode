bcaf5110e717cb39f5d0b84dd0e963af26fab6c4
Bruflot <bruflotdev@gmail.com>
2015-12-14 23:55:47 +0100

    Day 4

---
+++ 04/four_a.py
@@ -0,0 +1,14 @@
+from hashlib import md5
+
+secret = 'iwrupvqb'
+i = 0
+
+while True:
+    _hash = md5()
+    _hash.update(str(secret + str(i)).encode('utf-8'))
+    _hex = _hash.hexdigest()
+    if _hex[:5] == '00000':
+        break
+    i += 1
+
+print(i)
