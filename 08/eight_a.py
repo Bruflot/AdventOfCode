0d1413dc41dca51196af7fd66989fac907fa28ec
Bruflot <bruflotdev@gmail.com>
2015-12-22 01:20:36 +0100

    Day 8

---
+++ 08/eight_a.py
@@ -0,0 +1,15 @@
+import re
+regex = ('(\\\\\\\\)', '(\\\\x[0-9a-z]{2})', '(\\\\")')
+char_len = 0
+
+with open('eight_raw.txt', 'r') as f:
+    for line in f:
+        char = line.strip()
+        char_len += len(char)
+
+        for i in regex:
+            char = re.sub(i, '1', char)
+
+        char_len -= len(char.strip('"'))
+
+print(char_len)
