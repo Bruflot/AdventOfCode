ef3bd889eaf4233a6be7c0fe3fa8bdf7d2772076
Bruflot <bruflotdev@gmail.com>
2015-12-25 23:07:17 +0100

    Day 10

---
+++ 10/ten_a.py
@@ -0,0 +1,21 @@
+out = '1113122113'
+
+
+def output(_in):
+    n = 1
+    new_str = ''
+    for index, char in enumerate(_in):
+        if index+1 < len(_in):
+            if _in[index+1] == char:
+                n += 1
+                continue
+
+        new_str += str(n) + char
+        n = 1
+
+    return new_str
+
+for i in range(0, 50):
+    out = output(out)
+
+print(len(out))
