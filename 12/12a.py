19d15d283b6c8b67407aff762e80c6c68095bcd9
Bruflot <bruflotdev@gmail.com>
2015-12-28 20:35:22 +0100

    Day 12

--- 12/12a.py
+++ 12/12a.py
@@ -1,4 +1,4 @@
 import re
 
 with open('input.txt', 'r') as f:
-    print(sum(int(x) for x in re.findall(r'\-?[0-9]+', f.read())))
+    print(sum(int(x) for x in re.findall(r'\-?[\d]+', f.read())))
--- 12/12b.py
+++ 12/12b.py
@@ -1,21 +1,17 @@
-import re
+import json
+
+
+def parse(data):
+    if type(data) is int:
+        return data
+    elif type(data) is list:
+        return sum(map(parse, data))
+    elif type(data) is not dict:
+        return 0
+    elif 'red' in data.values():
+        return 0
+
+    return parse(list(data.values()))
 
 with open('input.txt', 'r') as f:
-    total = 0
-    buf = f.read()
-    braces = []
-    result = []
-    i = 0
-
-    for idx, char in enumerate(buf):
-        if char == '{':
-            braces.append(idx)
-        elif char == '}':
-            result.append([braces[-1], idx])
-
-    for i in result:
-        temp = buf[i[0]:i[1]]
-        if 'red' not in temp:
-            total += sum(int(x) for x in re.findall(r'\-?[0-9]+', temp))
-
-print(total)
+    print(parse(json.load(f)))
